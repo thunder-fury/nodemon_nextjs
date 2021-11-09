951368fad171464021d9b10818351e03e4ae66cf
禹賢祐 <whw3312@gmail.com>
2021-11-09 11:51:44 +0900

    no message

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,21 +3,22 @@ import { Color } from '../styles/Color'
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux'
 import { sp } from '../styles/Media'
+import axios from 'axios';
 interface props {}
 export const Index: React.FC<props> = ({
 }) => {
   const buttonElm = useRef(null)
   const dispatch = useDispatch()
   const [ businessDatas, setBusinessData ] = useState([])
-  // useEffect(() => {
-  //   const getBusinessData = async () => {
-  //     const endpoint ='/api/allData'
-  //     const res = await axios.get(getApiUrl())
-  //     console.log(res)
-  //     setBusinessData(res.data.allData)
-  //   }
-  //   getBusinessData()
-  // },[])
+  useEffect(() => {
+    const getBusinessData = async () => {
+      const endpoint ='http://localhost:3090/api/articles'
+      const res = await axios.get(endpoint)
+      console.log(res)
+      // setBusinessData(res.data.allData)
+    }
+    getBusinessData()
+  },[])
   return(
     <>
       トップページ
