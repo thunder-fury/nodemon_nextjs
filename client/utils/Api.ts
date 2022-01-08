444ccaff88b2c16dc3d50e05ef5ec34e61aadad5
禹賢祐 <whw3312@gmail.com>
2022-01-08 22:59:45 +0900

    APIフェッチロジック

--- client/utils/Api.ts
+++ client/utils/Api.ts
@@ -10,3 +10,87 @@ export const getFetch = (url: string): Promise<any> => {
       console.log(error)
     })
 }
+
+
+export const FetchGet = (
+  fetchInfo: { endPoint: string, basicAuth?: { id: string, password: string }, token?: string },
+): Promise<any> | undefined => {
+  const method = `GET`
+  const { endPoint, basicAuth, token } = fetchInfo
+  const headers: { [key: string]: string } = basicAuth ? {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${Buffer.from(`${basicAuth.id}:${basicAuth.password}`, 'binary').toString('base64')}`
+  } : token ? {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`
+  } : {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  return fetch(endPoint, { method, headers })
+    .then(res => {
+      return res.json()
+    })
+    .then(json => {
+      return json
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const FetchPost = (
+  endPoint: string,
+  posts: { [key: string]: string | number },
+  token?: string
+): Promise<any> => {
+  const method = `POST`
+  const headers: any = token ? {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  } : {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  const body = JSON.stringify(posts)
+  return fetch(endPoint, { method, headers, body })
+    .then(res => {
+      return res.json()
+    })
+    .then(json => {
+      return json
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+export const FetchPut = (
+  endPoint: string,
+  posts: { [key: string]: string | number },
+  token?: string
+): Promise<any> => {
+  const method = `PUT`
+  const headers: any = token ? {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  } : {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  const body = JSON.stringify(posts)
+  return fetch(endPoint, { method, headers, body })
+    .then(res => {
+      return res.json()
+    })
+    .then(json => {
+      return json
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
