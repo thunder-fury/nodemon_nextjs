9f76a27ca80fde06b4cd59460a3f0c61c29b98ea
禹賢祐 <whw3312@gmail.com>
2022-01-12 22:29:37 +0900

    出退勤ロジック調整

--- api/routes/punch.js
+++ api/routes/punch.js
@@ -7,35 +7,31 @@ const punch = (app) => {
   app.post(`/api/punch`, (req, res) => {
     const { active, attendance, leaving, date, member_id, note } = req.body;
     const allPunchSql = `SELECT * FROM punch`
-    const sql = `INSERT INTO punch VALUES (null, ?, ?, ?, ?, ?, ?)`;
-    const params = [active, attendance, leaving, date, member_id, note];
-    database().connect()
     database().query(allPunchSql, (error, results, fields) => {
-      const user = results.find(
-        (e) => Number(e.member_id) === member_id && dateFormat(e.date) === dateFormat(new Date())
-      )
-      console.log(results.length <= 0)
-      if(results.length <= 0) {
+      const user = results.find((e) => Number(e.member_id) === member_id && dateFormat(e.date) === dateFormat(new Date()))
+      if(user === undefined) {
+        const sql = `INSERT INTO punch VALUES (null, ?, ?, ?, ?, ?, ?)`;
+        const params = [active, attendance, leaving, date, member_id, note];
         database().query(sql, params,(err, rows, fields) => {
           res.header(`Content-Type`, `application/json; charset=utf-8`);
-          res.status(200).send({
-            status: 200,
-            success_messge : `出勤されました。本日も頑張りましょう！`,
-            respons: results
-          });
+            res.status(200).send({
+              status: 200,
+              success_messge : `出勤されました。本日も頑張りましょう！`,
+              respons: results
+            });
         });
       } else {
         if(user.leaving === `00:00:00`) {
           const update = `UPDATE punch SET leaving = ? where id = ${user.id}`
           database().query(update, leaving,(err, rows, fields) => {
-          res.header(`Content-Type`, `application/json; charset=utf-8`);
-          res.status(200).send({
-            status: 200,
-            success_messge : `退勤されました。本日もお疲れ様でした！`,
-            respons: results
+            res.header(`Content-Type`, `application/json; charset=utf-8`);
+            res.status(200).send({
+              status: 200,
+              success_messge : `退勤されました。本日もお疲れ様でした！`,
+              respons: results
+            });
           });
-        });
-        } else  {
+        } else {
           res.status(500).send({
             status: 500,
             error_messge : `本日の打刻はできません。明日頑張りましょう`,
