af774ee1740098af37827f0927a372f625bacb21
禹賢祐 <whw3312@gmail.com>
2022-01-14 21:44:26 +0900

    list 習得

--- client/src/components/Atoms/Table/index.tsx
+++ client/src/components/Atoms/Table/index.tsx
@@ -1,21 +1,26 @@
-import * as React from 'react'
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import { FIXME } from '../../../types/Any'
-import { Box, Typography } from '@mui/material'
+import { Box, TableBody, TableRow, Typography } from '@mui/material'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAsyncPunchGet, punchRes } from '../../../stores/slices/punchSlice'
 
 const columns: GridColDef[] = [
-  { field: 'date', headerName: '日時', width: 200 },
+  { field: 'f_date', headerName: '日時', width: 200 },
   // { field: 'active', headerName: '現在', width: 100 },
   { field: 'attendance', headerName: '出勤時間', width: 110 },
   { field: 'leaving', headerName: '退勤時間', width: 110 },
   { field: 'note', headerName: '備考', width: 110 },
 ]
 
-interface DataProps {
-  data: FIXME
-}
-
-const DataTable: React.FC<DataProps> = ({ data }) => {
+const DataTable: React.FC = () => {
+  const _punchRes = useSelector(punchRes)
+  const { respons } = _punchRes
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchAsyncPunchGet())
+  }, [])
+  console.log(respons)
   return (
     <Box
       sx={{
@@ -26,13 +31,13 @@ const DataTable: React.FC<DataProps> = ({ data }) => {
     >
       <Box
         sx={{
-          width: 500,
-          height: 300,
+          width: 550,
+          height: 500,
         }}
       >
         <Typography variant={`subtitle1`}>出勤リスト</Typography>
         <DataGrid
-          rows={data && data}
+          rows={respons && respons}
           columns={columns}
           // pageSize={5}
           // rowsPerPageOptions={[5]}
