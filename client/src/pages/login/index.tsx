12393a78d6d177bd47865cac12344883040c5986
禹賢祐 <whw3312@gmail.com>
2022-01-18 16:22:47 +0900

    login signup block コンポーネント化

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,89 +1,20 @@
-import { useEffect, useState } from 'react'
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  CardHeader,
-  TextField,
-  Typography,
-} from '@mui/material'
-import {
-  fetchAsyncLogin,
-  loginRes,
-  setVal,
-  loginLoading,
-} from '../../stores/slices/loginSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import ModalGrid from '../../components/Atoms/Modal'
-import { FIXME } from '../../types/Any'
-import { getSesstion } from '../../utils/Sesstion'
+import { Box, Button, Card, Modal, Typography } from '@mui/material'
 import Router from 'next/router'
-import dynamic from 'next/dynamic'
-const Loading = dynamic(() => import('../../components/Atoms/loading'), {
-  ssr: false,
-})
-export const Login = () => {
-  const [userEmeil, setUserEmail] = useState(``)
-  const [userPassword, setUserPassword] = useState(``)
-  const dispatch = useDispatch()
-  const _loginRes = useSelector(loginRes)
-  const _loginLoading = useSelector(loginLoading)
-  const { status }: FIXME = _loginRes
-  const login = async () => {
-    const data = {
-      email: userEmeil,
-      password: userPassword,
-    }
-    dispatch(fetchAsyncLogin(data))
-  }
-  useEffect(() => {
-    getSesstion(`role`) && Router.push(`/my_page`)
-  }, [])
+import LoginBlock from '../../components/Block/Login'
 
+export const Login = () => {
   return (
     <>
-      {status && <ModalGrid res={_loginRes} setVal={setVal} />}
-      <Loading open={_loginLoading} />
-      <Typography variant="h4" gutterBottom component="div">
-        Login
-      </Typography>
-      <Box display={`flex`} justifyContent={`center`}>
-        <Box width={500} display={`flex`} flexDirection={`column`}>
-          <TextField
-            fullWidth
-            id={`outlined-basic`}
-            label={`email`}
-            variant={`outlined`}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setUserEmail(e.target.value)
-            }}
-          />
-          <br />
-          <TextField
-            fullWidth
-            id={`outlined-basic`}
-            label={`password`}
-            type={`password`}
-            variant={`outlined`}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setUserPassword(e.target.value)
-            }}
-          />
-          <br />
-        </Box>
-      </Box>
-      <Box display={`flex`} justifyContent={`center`} mt={2}>
-        <Box width={400}>
-          <ButtonGroup
-            fullWidth
-            variant={`contained`}
-            aria-label={`outlined primary button group`}
-          >
-            <Button size={`large`} color={`primary`} fullWidth onClick={login}>
-              Login
-            </Button>
-          </ButtonGroup>
-        </Box>
+      <LoginBlock />
+      <Box sx={{ textAlign: 'center' }}>
+        <Typography
+          sx={{ fontSize: 15, mt: 1 }}
+          color="text.secondary"
+          gutterBottom
+        >
+          ユーザー登録されてない方は登録が必要です。
+        </Typography>
+        <Button onClick={() => Router.push(`/signup`)}>Move Signup Page</Button>
       </Box>
     </>
   )
--- client/src/pages/signup/index.tsx
+++ client/src/pages/signup/index.tsx
@@ -1,95 +1,20 @@
-import { useEffect, useRef, useState } from 'react'
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  CardHeader,
-  CircularProgress,
-  Modal,
-  TextField,
-  Typography,
-} from '@mui/material'
-import { css } from '@emotion/react'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-  fetchAsyncSignup,
-  signupRes,
-  signupLoading,
-  setVal,
-} from '../../stores/slices/signupSlice'
+import { Box, Button, ButtonGroup, Typography } from '@mui/material'
 import Router from 'next/router'
-import { FIXME } from '../../types/Any'
-import ModalGrid from '../../components/Atoms/Modal'
+import SignUpBlock from '../../components/Block/SignUp'
+
 export const SignUp = () => {
-  const [userEmeil, setUserEmail] = useState(``)
-  const [userPassword, setUserPassword] = useState(``)
-  const [userName, setUserName] = useState(``)
-  const dispatch = useDispatch()
-  const _signupRes = useSelector(signupRes)
-  const _signupLoading = useSelector(signupLoading)
-  const [open, setOpen] = useState(false)
-  const { status }: FIXME = _signupRes
-  const submit = async () => {
-    const data = {
-      email: userEmeil,
-      password: userPassword,
-      user_name: userName,
-    }
-    dispatch(fetchAsyncSignup(data))
-  }
   return (
     <>
-      {_signupLoading && <CircularProgress />}
-      {status && <ModalGrid res={_signupRes} setVal={setVal} />}
-      <Typography variant="h4" gutterBottom component="div">
-        SignUp
-      </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Box sx={{ width: 500, display: 'flex', flexDirection: 'column' }}>
-          <TextField
-            fullWidth
-            id={`outlined-basic`}
-            label={`name`}
-            variant={`outlined`}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setUserName(e.target.value)
-            }}
-          />
-          <br />
-          <TextField
-            fullWidth
-            id={`outlined-basic`}
-            label={`email`}
-            variant={`outlined`}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setUserEmail(e.target.value)
-            }}
-          />
-          <br />
-          <TextField
-            fullWidth
-            id={`outlined-basic`}
-            label={`password`}
-            type={`password`}
-            variant={`outlined`}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setUserPassword(e.target.value)
-            }}
-          />
-        </Box>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', m: 2 }}>
-        <Box sx={{ width: 400 }}>
-          <ButtonGroup
-            fullWidth
-            variant={`contained`}
-            aria-label={`outlined primary button group`}
-          >
-            <Button size={`large`} color={`primary`} fullWidth onClick={submit}>
-              Signup
-            </Button>
-          </ButtonGroup>
-        </Box>
+      <SignUpBlock />
+      <Box sx={{ textAlign: 'center' }}>
+        <Typography
+          sx={{ fontSize: 15, mt: 1 }}
+          color="text.secondary"
+          gutterBottom
+        >
+          ユーザー登録されている方はログインページへ。
+        </Typography>
+        <Button onClick={() => Router.push(`/login`)}>Move Login Page</Button>
       </Box>
     </>
   )
