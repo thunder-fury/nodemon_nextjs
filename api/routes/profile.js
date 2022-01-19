07094f1bbdb5228e8e5d660e0f15347b26ddec79
禹賢祐 <whw3312@gmail.com>
2022-01-19 21:12:08 +0900

    プロフィールアップデートAPI

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -15,15 +15,37 @@ const profile = (app) => {
     `/api/profile_update`,
     uploadWithOriginalFilename.single('image'),
     (req, res) => {
-      console.log(res)
-      const { member_id } = req.body
-      const sql = `UPDATE member SET image = ? where id = ${member_id}`
-      const image = `http://${req.headers.host}/image/${req.file.filename}`;
+      const { member_id, user_name } = req.body
+      const getUser = `SELECT * FROM member WHERE id = ${member_id}`
       database().connect();
-      database().query(sql, image, (err, rows, fields) => {
-        res.header(`Content-Type`, `application/json; charset=utf-8`);
-        res.status(200).send({ reow: rows });
-      });
+      if(req.file) {
+        const sql = `UPDATE member SET image = ?, user_name = ? where id = ${member_id}`
+        const image = `http://${req.headers.host}/image/${req.file.filename}`;
+        const params = [image, user_name]
+        database().query(sql, params, (err, rows, fields) => {
+          res.header(`Content-Type`, `application/json; charset=utf-8`);
+          database().query(getUser, (error, results, fields) => {
+            res.status(200).send({ status:200, respons: results, success_messge: `success` });
+          })
+        });
+      } else {
+        const sql = `UPDATE member SET user_name = ? where id = ${member_id}`
+        database().query(sql, user_name, (err, rows, fields) => {
+          res.header(`Content-Type`, `application/json; charset=utf-8`);
+          database().query(getUser, (error, results, fields) => {
+            res.status(200).send({ status:200, respons: results, success_messge: `success` });
+          })
+        });
+      }
+      // if(user_name) {
+      // } else {
+      //   res.status(500).send({
+      //     status: 500,
+      //     error_messge : `本日の打刻はできません。明日頑張りましょう`,
+      //     respons: results
+      //   });
+      // }
+
       database().end();
     },
   );
